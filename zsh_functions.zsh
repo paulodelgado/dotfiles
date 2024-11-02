ab751bb1abfc969115180a1e8706dcc92ff0360f
Paulo Delgado <paulo@paulodelgado.com>
2024-11-02 11:06:22 -0700

    Cleanup 2024

--- zsh_functions.zsh
+++ zsh_functions.zsh
@@ -1,12 +1,16 @@
 # zsh: Place this in .zshrc after "source /Users/georgen/.iterm2_shell_integration.zsh".
-iterm2_print_user_vars() {
-  iterm2_set_user_var gitBranch $((git branch 2> /dev/null) | grep \* | cut -c3-)
-}
+#iterm2_print_user_vars() {
+#  iterm2_set_user_var gitBranch $((git branch 2> /dev/null) | grep \* | cut -c3-)
+#}
 
 alias pwd_name='basename $PWD'
 
 work() {
-  iterm2_set_user_var wname `pwd_name`
   tmux new -s `pwd_name`
+}
+
+voapps() {
+  cd ~/Repositories/VoApps/$1
+  work
 }
 
